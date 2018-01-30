446375deddcb9da695e4c91f4bd26e75ade15227
alessandro.souza <alessand.souza@gmail.com>
2018-01-30 11:20:58 -0200

    Add project files.

---
+++ BankRef/Infrastruture/WepAppication.cs
@@ -0,0 +1,29 @@
+﻿// 
+
+using System;
+using System.Net;
+
+namespace BankRef.Infrastruture
+{
+    public class WepAppication
+    {
+        private readonly string[] _internalUrlsStrings;
+
+        public WepAppication(string[] urlsStrings)
+        {
+            if ( urlsStrings ==null)
+                throw new ArgumentException(nameof(urlsStrings));
+            _internalUrlsStrings = urlsStrings;
+
+        }
+
+
+        public void Initiallize()
+        {
+            var httpListener = new HttpListener();
+            foreach (var internalUrlsString in _internalUrlsStrings)
+                httpListener.Prefixes.Add(internalUrlsString);
+            httpListener.Start();
+        }
+    }
+}
---
+++ BankRef/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankRef.Infrastruture;
+
+namespace BankRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var urlApplication = new string[]{"http://localhost:5341"};
+            var webApp = new WepAppication(urlApplication);
+
+
+        }
+    }
+}
